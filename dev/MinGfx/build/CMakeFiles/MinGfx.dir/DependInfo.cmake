76e0593b99b5bc04f9d6eb9f480165d70220b045
Warren <weber767@umn.edu>
2020-11-27 16:25:04 -0600

    cleanup

---
+++ dev/MinGfx/build/CMakeFiles/MinGfx.dir/DependInfo.cmake
@@ -0,0 +1,50 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/aabb.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/aabb.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/bvh.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/bvh.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/color.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/color.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/craft_cam.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/craft_cam.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/default_shader.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/default_shader.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/gfxmath.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/gfxmath.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/graphics_app.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/graphics_app.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/matrix4.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/matrix4.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/mesh.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/mesh.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/platform.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/platform.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/point2.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/point2.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/point3.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/point3.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/quaternion.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/quaternion.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/quick_shapes.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/quick_shapes.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/ray.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/ray.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/shader_program.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/shader_program.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/text_shader.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/text_shader.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/texture2d.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/texture2d.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/unicam.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/unicam.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/vector2.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/vector2.cc.o"
+  "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/src/vector3.cc" "/home/sender/csci/csci4611/repo-weber767/dev/MinGfx/build/CMakeFiles/MinGfx.dir/src/vector3.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LINUX"
+  "USE_NanoGUI"
+  "USE_OPENGL"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "/home/sender/csci/csci4611/repo-weber767/include"
+  "/home/sender/csci/csci4611/repo-weber767/include/nanovg"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
